aaa3881acc884491c53b51aa4e469e9eeb2eeff2
ray <kjwdev01@gmail.com>
2024-05-22 08:12:38 +0900

    update

--- .config/nvim/lua/plugins/utils/telescope-fzf-native.lua
+++ .config/nvim/lua/plugins/utils/telescope-fzf-native.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
-    init = function ()
+    build = 'make',
+    config = function ()
         local telescope = require('telescope')
         telescope.load_extension("fzf")
     end
