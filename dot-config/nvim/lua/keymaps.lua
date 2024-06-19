4ed82fbdfc5788df5705d8792ef40029ece7cee7
ray <kjwdev01@gmail.com>
2024-06-20 07:53:32 +0900

    Update: Add Keymaps, etc.

--- dot-config/nvim/lua/keymaps.lua
+++ dot-config/nvim/lua/keymaps.lua
@@ -1,5 +1,5 @@
 local function map(mode, lhs, rhs, opts)
-    local default_opts = { silent = true, noremap = false }
+    local default_opts = { silent = true, noremap = true }
 
     if opts ~= nil then
         for k, v in pairs(opts) do default_opts[k] = v end
@@ -10,24 +10,27 @@ end
 
 
 -- Windows
-map("", "<leader>wh", "<C-w>h", { desc = "Move to Left Window" })
-map("", "<leader>wj", "<C-w>j", { desc = "Move to Down Window" })
-map("", "<leader>wk", "<C-w>k", { desc = "Move to Up Window" })
-map("", "<leader>wl", "<C-w>l", { desc = "Move to Right Window" })
+map("", "<C-h>", "<C-w>h", { desc = "Move to Left Window" })
+map("", "<C-j>", "<C-w>j", { desc = "Move to Down Window" })
+map("", "<C-k>", "<C-w>k", { desc = "Move to Up Window" })
+map("", "<C-l>", "<C-w>l", { desc = "Move to Right Window" })
+map("", "<leader>pc", "<C-w>c", { desc = "Close Window" })
+map("", "<leader>pm", "<C-w>o", { desc = "Maximize Window" })
 map("", "<leader>sh", ":vsplit<CR>", { desc = "Split Window to the Left" })
 map("", "<leader>sj", ":split<CR><C-w>j", { desc = "Split Window to the Bottom" })
 map("", "<leader>sk", ":split<CR>", { desc = "Split Window to the Top" })
 map("", "<leader>sl", ":vsplit<CR><C-w>l", { desc = "Split Window to the Right" })
-map("", "<leader>wc", "<C-w>c", { desc = "Close Window" })
-map("", "<leader>wo", "<C-w>o", { desc = "Maximize Window" })
 
 -- General
 map("", "<leader>qq", "<cmd>confirm qa<cr>", { desc = "Exit NeoVim" })
 map("", "<leader>oc", "<cmd>e ~/.config/nvim<cr>", { desc = "Open Neovim Config" })
+map("", "<leader>wb", "<cmd>w<cr>", { desc = "Write to Buffer" })
+map("", "<leader>wa", "<cmd>wa<cr>", { desc = "Write All" })
 
 -- Editing
 map("", "<leader>y", "\"+y", { desc = "Yank to Clipboard" })
 map("i", "<C-C>", "<ESC>")
+
 map("v", "J", function ()
     return ":m '>" .. (vim.v.count > 1 and vim.v.count or 1) .."<CR>gv=gv"
 end, { expr = true, desc = "Move Selected Line Down" })
@@ -40,4 +43,3 @@ end, { expr = true, desc = "Move Selected Line Up" })
 -- File
 map("n", "<leader>bw", "<cmd>w<cr>", {desc = "[B]uffer [W]rite"})
 map("i", "kjl", "<Esc>")
-
