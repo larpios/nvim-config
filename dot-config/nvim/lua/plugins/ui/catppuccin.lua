154f77cb821f0ebe94c2644744a9e7747b904ab0
ray <kjwdev01@gmail.com>
2024-07-07 22:18:05 +0900

    Update: Nvim Make Colorscheme Transparent

---
+++ dot-config/nvim/lua/plugins/ui/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+        transparent_background = true,
+        styles = {
+            sidebars = "transparent",
+            floats = "transparent",
+        },
+    },
+}
