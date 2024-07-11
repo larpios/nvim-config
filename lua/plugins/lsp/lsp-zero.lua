f432f5c691335b935d1b38906d72046942c238a4
ray <kjwdev01@gmail.com>
2024-07-11 09:04:49 +0900

    Update: Add tmux integraton

--- lua/plugins/lsp/lsp-zero.lua
+++ lua/plugins/lsp/lsp-zero.lua
@@ -4,8 +4,8 @@ return {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
     dependencies = {
-        { 'hrsh7th/cmp-nvim-lsp' },
         { 'neovim/nvim-lspconfig' },
+        { 'hrsh7th/cmp-nvim-lsp' },
         { 'hrsh7th/nvim-cmp' },
         { 'L3MON4D3/LuaSnip' },
         { 'williamboman/mason.nvim' },
@@ -13,6 +13,8 @@ return {
     },
     config = function()
         local lsp_zero = require('lsp-zero')
+
+        lsp_zero.setup({})
 
         lsp_zero.on_attach(function(client, bufnr)
             lsp_zero.default_keymaps({ buffer = bufnr, exclude = { '<F2>', '<F3>','<F4>'} })
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,8 +1,5 @@
 return {
     "williamboman/mason.nvim",
-    keys = {
-        { "<leader>m", ":Mason<CR>", noremap = true, silent = true, desc = "Mason" }
-    },
     config = function()
         require("mason").setup()
     end
