57ce79242e5c0b489a3ca76b650b7bd15356e942
ray <kjwdev01@gmail.com>
2024-08-08 22:52:11 +0900

    update: swap bookmarks to a different plugin

--- lua/plugins/utils/bookmarks.lua
+++ lua/plugins/utils/bookmarks.lua
@@ -1,13 +1,16 @@
+-- with lazy.nvim
 return {
-    'crusj/bookmarks.nvim',
-    enabled = false,
-    keys = {
-        { "<tab><tab>", mode = { "n" } },
-    },
-    branch = 'main',
-    dependencies = { 'nvim-web-devicons' },
-    config = function()
-        require("bookmarks").setup()
-        require("telescope").load_extension("bookmarks")
-    end,
+  'LintaoAmons/bookmarks.nvim',
+  -- tag = "v0.5.4", -- optional, pin the plugin at specific version for stability
+  dependencies = {
+    { 'nvim-telescope/telescope.nvim' },
+    { 'stevearc/dressing.nvim' }, -- optional: to have the same UI shown in the GIF
+  },
+
+  config = function()
+    vim.keymap.set({ 'n', 'v' }, 'mm', '<cmd>BookmarksMark<cr>', { desc = 'Mark current line into active BookmarkList.' })
+    vim.keymap.set({ 'n', 'v' }, 'mo', '<cmd>BookmarksGoto<cr>', { desc = 'Go to bookmark at current active BookmarkList' })
+    vim.keymap.set({ 'n', 'v' }, 'ma', '<cmd>BookmarksCommands<cr>', { desc = 'Find and trigger a bookmark command.' })
+    vim.keymap.set({ 'n', 'v' }, 'mg', '<cmd>BookmarksGotoRecent<cr>', { desc = 'Go to latest visited/created Bookmark' })
+  end,
 }
