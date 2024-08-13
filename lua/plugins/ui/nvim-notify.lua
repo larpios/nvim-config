1f2542f802ea24b58c9f0d39a2ac3952eb7e0b7f
ray <kjwdev01@gmail.com>
2024-08-13 09:12:29 +0900

    update: add nvim-notify config

--- lua/plugins/ui/nvim-notify.lua
+++ lua/plugins/ui/nvim-notify.lua
@@ -1,1 +1,23 @@
-return {'rcarriga/nvim-notify'}
+return {
+    'rcarriga/nvim-notify',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+        require('notify').setup({
+            render = 'compact',
+            stages = 'slide',
+            timeout = 5000,
+            background_colour = '#000000',
+            icons = {
+                ERROR = '',
+                WARN = '',
+                INFO = '',
+                DEBUG = '',
+                TRACE = '✎',
+            },
+        })
+        vim.keymap.set('n', '<leader>fn', '<cmd>Telescope notify<cr>', {desc = "Find Notify History"})
+    end
+}
