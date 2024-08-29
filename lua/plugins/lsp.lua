e8407fa3bd628e0fb263b2e722b1ee10b7a1bd3d
ray <kjwdev01@gmail.com>
2024-08-29 23:07:31 +0900

    fix: fzf-lua fc

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,234 +1,201 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = { 'c', 'cpp', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'rust' },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+                ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust" },
 
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
 
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
 
-        -- List of parsers to ignore installing (or "all")
-        ignore_install = { 'javascript' },
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+                highlight = {
+                    enable = true,
 
-        highlight = {
-          enable = true,
+                    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
 
-          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-          -- the name of the parser)
-          -- list of language that will be disabled
-          disable = { 'c', 'rust' },
-          -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-          disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
-
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
-      }
-    end,
-  },
-  {
-    'stevearc/conform.nvim',
-    opts = {},
-    config = function()
-      require('conform').setup {
-        formatters_by_ft = {
-          lua = { 'stylua' },
-          python = { 'black' },
-          rust = { 'rustfmt' },
-          c = { 'clang_format' },
-          cpp = { 'clang_format' },
-          cmake = { 'cmake_format' },
-        },
-      }
-
-      vim.keymap.set('n', '<leader>cf', "<cmd>lua require('conform').format()<cr>")
-    end,
-  },
-
-  {
-    'mrcjkb/rustaceanvim',
-    version = '^5', -- Recommended
-    lazy = false, -- This plugin is already lazy
-  },
-
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v4.x',
-    dependencies = {
-      { 'neovim/nvim-lspconfig' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-nvim-lua' },
-      { 'L3MON4D3/LuaSnip' },
-      { 'onsails/lspkind.nvim' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
-      { 'mrcjkb/rustaceanvim' },
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end,
     },
-    config = function()
-      local lsp_zero = require 'lsp-zero'
-      local cmp = require 'cmp'
-      local cmp_action = lsp_zero.cmp_action()
-      local cmp_format = lsp_zero.cmp_format { details = true }
+    {
+        "stevearc/conform.nvim",
+        opts = {},
+        config = function()
+            require("conform").setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    python = { "black" },
+                    rust = { "rustfmt" },
+                    c = { "clang_format" },
+                    cpp = { "clang_format" },
+                    cmake = { "cmake_format" },
+                },
+            })
 
-      cmp.setup {
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'buffer' },
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        formatting = {
-          fields = { 'abbr', 'kind', 'menu' },
-          format = require('lspkind').cmp_format {
-            mode = 'symbol', -- show only symbol annotations
-            maxwidth = 50, -- prevent the popup from showing more than provided characters
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-          },
-        },
-        mapping = {
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.close(),
-          ['<C-y>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-          },
-        },
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
-      }
+            vim.keymap.set("n", "<leader>cf", "<cmd>lua require('conform').format()<cr>")
+        end,
+    },
 
-      lsp_zero.setup {}
+    {
+        "mrcjkb/rustaceanvim",
+        version = "^5", -- Recommended
+        lazy = false, -- This plugin is already lazy
+    },
 
-      lsp_zero.on_attach(function(client, bufnr)
-        lsp_zero.default_keymaps { buffer = bufnr, exclude = { '<F2>', '<F3>', '<F4>' } }
-      end)
-
-      lsp_zero.extend_lspconfig {
-        capabilities = require('cmp_nvim_lsp').default_capabilities(),
-      }
-
-      vim.g.rustaceanvim = {
-        server = {
-          capabilities = lsp_zero.get_capabilities(),
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v4.x",
+        dependencies = {
+            { "neovim/nvim-lspconfig" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-nvim-lua" },
+            { "L3MON4D3/LuaSnip" },
+            { "onsails/lspkind.nvim" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
+            { "mrcjkb/rustaceanvim" },
         },
-      }
+        config = function()
+            local lsp_zero = require("lsp-zero")
+            local cmp = require("cmp")
+            local cmp_action = lsp_zero.cmp_action()
+            local cmp_format = lsp_zero.cmp_format({ details = true })
 
-      require('mason').setup {
-        ui = {
-          icons = {
-            package_installed = '✓',
-            package_pending = '➜',
-            package_uninstalled = '✗',
-          },
-        },
-      }
-      require('mason-lspconfig').setup {
-        ensure_installed = { 'lua_ls', 'rust_analyzer', 'clangd' },
-        handlers = {
-          function(server_name)
-            require('lspconfig')[server_name].setup {}
-          end,
-          lua_ls = function()
-            require('lspconfig').lua_ls.setup {
-              on_init = function(client)
-                lsp_zero.nvim_lua_settings(client, {})
-              end,
+            cmp.setup({
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                formatting = {
+                    fields = { "abbr", "kind", "menu" },
+                    format = require("lspkind").cmp_format({
+                        mode = "symbol", -- show only symbol annotations
+                        maxwidth = 50, -- prevent the popup from showing more than provided characters
+                        ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+                    }),
+                },
+                mapping = {
+                    ["<C-n>"] = cmp.mapping.select_next_item(),
+                    ["<C-p>"] = cmp.mapping.select_prev_item(),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.close(),
+                    ["<C-y>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = true,
+                    }),
+                },
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+            })
+
+            lsp_zero.setup({})
+
+            lsp_zero.on_attach(function(client, bufnr)
+                lsp_zero.default_keymaps({ buffer = bufnr, exclude = { "<F2>", "<F3>", "<F4>" } })
+            end)
+
+            lsp_zero.extend_lspconfig({
+                capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            })
+
+            vim.g.rustaceanvim = {
+                server = {
+                    capabilities = lsp_zero.get_capabilities(),
+                },
             }
-          end,
-          rust_analyzer = lsp_zero.noop,
-        },
-      }
-    end,
-  },
-  {
-    'neovim/nvim-lspconfig',
-  },
-  {
-    'nvimdev/lspsaga.nvim',
-    config = function()
-      require('lspsaga').setup {}
-      require('lspsaga.symbol.winbar').get_bar()
-    end,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter', -- optional
-      'nvim-tree/nvim-web-devicons', -- optional
-    },
-  },
 
-  {
-    'williamboman/mason.nvim',
-    dependencies = {
-      'williamboman/mason-lspconfig.nvim',
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "rust_analyzer",
+                    "clangd",
+                    "bashls",
+                    "html",
+                    "cssls",
+                    "jsonls",
+                    "yamlls",
+                    "tsserver",
+                    "bashls",
+                    "pylsp",
+                    "pylyzer",
+                    "pyright",
+                    "rust_analyzer",
+                    "clangd",
+                    "cmake",
+                    "kotlin_language_server",
+                    "vimls",
+                    "marksman",
+                    "graphql",
+                    "lua_ls",
+                },
+                automatic_installation = true,
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({})
+                    end,
+                    lua_ls = function()
+                        require("lspconfig").lua_ls.setup({
+                            on_init = function(client)
+                                lsp_zero.nvim_lua_settings(client, {})
+                            end,
+                        })
+                    end,
+                    rust_analyzer = lsp_zero.noop,
+                },
+            })
+        end,
     },
-    config = function()
-      require('mason').setup()
-      require('mason-lspconfig').setup {
-        ensure_installed = {
-          'html',
-          'cssls',
-          'jsonls',
-          'yamlls',
-          'tsserver',
-          'bashls',
-          'pylsp',
-          'pylyzer',
-          'pyright',
-          'rust_analyzer',
-          'clangd',
-          'cmake',
-          'kotlin_language_server',
-          'vimls',
-          'marksman',
-          'graphql',
-          'lua_ls',
+    {
+        "nvimdev/lspsaga.nvim",
+        config = function()
+            require("lspsaga").setup({})
+            require("lspsaga.symbol.winbar").get_bar()
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter", -- optional
+            "nvim-tree/nvim-web-devicons", -- optional
         },
-      }
-    end,
-  },
-  {
-    'nvimtools/none-ls.nvim',
-    enabled = false,
-    config = function()
-      local null_ls = require 'null-ls'
-
-      null_ls.setup {
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.completion.spell,
-        },
-      }
-    end,
-  },
+    },
 }
