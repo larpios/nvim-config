863beb8bc42cb6e5a4f6ab05daa0e6a6bdd94484
ray <kjwdev01@gmail.com>
2024-09-11 12:32:10 +0900

    update: add stuff

--- lua/larp/plugins/utility.lua
+++ lua/larp/plugins/utility.lua
@@ -133,33 +133,6 @@ return {
     'mfussenegger/nvim-dap',
   },
   {
-    'debugloop/telescope-undo.nvim',
-    dependencies = {
-      {
-        'nvim-telescope/telescope.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-      },
-    },
-    keys = {
-      '<leader>fu',
-      '<cmd>Telescope undo<cr>',
-      desc = 'Undo History',
-    },
-    opts = {
-      extensions = {
-        undo = {
-          side_by_side = true,
-        },
-      },
-    },
-
-    config = function(_, opts)
-      local telescope = require('telescope')
-      telescope.setup(opts)
-      telescope.load_extension('undo')
-    end,
-  },
-  {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.6',
     dependencies = {
