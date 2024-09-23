b22a836788c747da180807446cc763e9786973c9
kjw <kjwdev01@gmail.com>
2024-09-24 01:32:25 +0900

    update: add codesnap plugin for code snapshot

--- lua/larp/plugins/utils.lua
+++ lua/larp/plugins/utils.lua
@@ -158,4 +158,19 @@ return {
             })
         end,
     },
+
+    -- Snapshop code
+    {
+        'mistricky/codesnap.nvim',
+        build = 'make build_generator',
+        keys = {
+            { '<leader><leader>cc', '<cmd>CodeSnap<cr>', mode = 'x', desc = 'Save selected code snapshot into clipboard' },
+            { '<leader><leader>cs', '<cmd>CodeSnapSave<cr>', mode = 'x', desc = 'Save selected code snapshot in ~/Pictures' },
+        },
+        opts = {
+            save_path = '~/Pictures',
+            has_breadcrumbs = true,
+            bg_theme = 'bamboo',
+        },
+    },
 }
