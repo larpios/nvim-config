266f4d19438824dea38298f9ac49d0d0c65c3aed
kjwsl <kjwdev01@gmail.com>
2024-10-13 23:26:59 +0900

    Fix orgmode journal keymap

--- lua/larp/plugins/integrations.lua
+++ lua/larp/plugins/integrations.lua
@@ -367,12 +367,14 @@ return {
             require('orgmode').setup({
                 org_agenda_files = org_path .. '/**/*',
                 org_default_notes_file = org_path .. 'refile.org',
+                org_fold_enable = false,
             })
             larp.fn.map('n', '<leader>oo', ':e ' .. org_path .. '<cr>', { desc = 'Open Orgmode' })
             larp.fn.map('n', '<leader>of', ':FzfLua files cwd=' .. org_path .. '<cr>', { desc = 'Find Org Files' })
             larp.fn.map('n', '<leader>oj', function()
                 local today = os.date('*t')
                 local journal = org_path .. '/journal/' .. today.year .. '/' .. today.month .. '/' .. today.day .. '.org'
+                vim.cmd('!mkdir -p ' .. org_path .. '/journal/' .. today.year .. '/' .. today.month)
                 vim.cmd('e ' .. journal)
             end, { desc = 'Open Org Journal' })
             -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
