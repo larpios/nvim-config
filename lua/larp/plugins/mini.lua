4a9d201bb9903e82af0f7ef78b1494116639eee6
kjwsl <kjwdev01@gmail.com>
2024-10-13 23:43:22 +0900

    mini.surround no longer overrides the mappings for flash.nvim keymappings

--- lua/larp/plugins/mini.lua
+++ lua/larp/plugins/mini.lua
@@ -11,6 +11,18 @@ return {
         require('mini.ai').setup({})
         require('mini.surround').setup({
             respect_selection_type = true,
+            mappings = {
+                add = 'Sa', -- Add surrounding in Normal and Visual modes
+                delete = 'Sd', -- Delete surrounding
+                find = 'Sf', -- Find surrounding (to the right)
+                find_left = 'SF', -- Find surrounding (to the left)
+                highlight = 'Sh', -- Highlight surrounding
+                replace = 'Sr', -- Replace surrounding
+                update_n_lines = 'Sn', -- Update `n_lines`
+
+                suffix_last = 'l', -- Suffix to search with "prev" method
+                suffix_next = 'n', -- Suffix to search with "next" method
+            },
         })
         require('mini.move').setup({
             mappings = {
