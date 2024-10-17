603f3e0317a7aa51b8d2883ffb339368d8d18e50
kjwsl <kjwdev01@gmail.com>
2024-10-17 09:26:05 +0900

    automatic fold

--- lua/larp/plugins/lsp.lua
+++ lua/larp/plugins/lsp.lua
@@ -337,7 +337,7 @@ return {
         dependencies = { 'kevinhwang91/promise-async' },
         init = function()
             vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-            vim.o.foldlevelstart = 99
+            vim.o.foldlevelstart = 1
             vim.o.foldenable = true
             vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
         end,
