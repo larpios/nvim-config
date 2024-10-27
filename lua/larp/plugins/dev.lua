6146d5bc9aee889cd11f6dad88a50377e1f5ad3e
kjwsl <kjwdev01@gmail.com>
2024-10-28 00:13:43 +0900

    update: Add overseer templates for AOSP and C++ projects

--- lua/larp/plugins/dev.lua
+++ lua/larp/plugins/dev.lua
@@ -2,25 +2,54 @@ return {
 
     {
         'stevearc/overseer.nvim',
+        dependencies = {
+            'akinsho/toggleterm.nvim',
+            'rmagatti/auto-session',
+        },
         opts = {},
         config = function()
             local overseer = require('overseer')
             overseer.setup({
                 templates = {
                     'builtin',
-                    -- 'user.cpp_build',
+                    'larp.cpp',
                 },
+                strategy = 'toggleterm',
             })
-            overseer.register_template('user.cpp_build', {
-                name = 'C++ cmake build',
-                cmd = { 'cmake ..', 'make' },
-                condition = {
-                    filetype = 'cpp',
-                    callback = function()
-                        return vim.fn.filereadable('CMakeLists.txt') == 1
+
+            -- Convert the cwd to a simple file name
+            local function get_cwd_as_name()
+                local dir = vim.fn.getcwd(0)
+                return dir:gsub('[^A-Za-z0-9]', '_')
+            end
+            local overseer = require('overseer')
+            require('auto-session').setup({
+                pre_save_cmds = {
+                    function()
+                        overseer.save_task_bundle(
+                            get_cwd_as_name(),
+                            -- Passing nil will use config.opts.save_task_opts. You can call list_tasks() explicitly and
+                            -- pass in the results if you want to save specific tasks.
+                            nil,
+                            { on_conflict = 'overwrite' } -- Overwrite existing bundle, if any
+                        )
+                    end,
+                },
+                -- Optionally get rid of all previous tasks when restoring a session
+                pre_restore_cmds = {
+                    function()
+                        for _, task in ipairs(overseer.list_tasks({})) do
+                            task:dispose(true)
+                        end
+                    end,
+                },
+                post_restore_cmds = {
+                    function()
+                        overseer.load_task_bundle(get_cwd_as_name(), { ignore_missing = true })
                     end,
                 },
             })
+
             larp.fn.map('n', '<leader>Ot', ':OverseerToggle<CR>', { desc = 'Toggle Overseer' })
             larp.fn.map('n', '<leader>Or', ':OverseerRun<CR>', { desc = 'Overseer Run' })
             larp.fn.map('n', '<leader>Ob', ':OverseerBuild<CR>', { desc = 'Overseer Build' })
@@ -53,10 +82,10 @@ return {
                 dapui.open()
             end
             dap.listeners.before.event_terminated.dapui_config = function()
-                dapui.open()
+                dapui.close()
             end
             dap.listeners.before.event_exited.dapui_config = function()
-                dapui.open()
+                dapui.close()
             end
 
             dap.adapters.gdb = {
@@ -66,7 +95,7 @@ return {
             }
             dap.adapters.lldb = {
                 type = 'executable',
-                command = 'lldb-vscode',
+                command = 'lldb-vscode-14',
                 name = 'lldb',
             }
             dap.configurations.cpp = {
@@ -89,10 +118,10 @@ return {
             dap.configurations.rust = dap.configurations.cpp
 
             larp.fn.map('n', '<leader>cdb', dap.toggle_breakpoint, { desc = 'Toggle breakpoint', desc_prefix = 'DAP' })
-            larp.fn.map('n', '<leader>cdr', dap.run, { desc = 'Continue', desc_prefix = 'DAP' })
-            larp.fn.map('n', '<leader>cdc', dap.continue, { desc = 'Continue', desc_prefix = 'DAP' })
-            larp.fn.map('n', '<F3>', dap.step_over, { desc = 'Step Over', desc_prefix = 'DAP' })
-            larp.fn.map('n', '<F4>', dap.step_into, { desc = 'Step Into', desc_prefix = 'DAP' })
+            larp.fn.map('n', '<leader>cdr', dap.run, { desc = 'Run', desc_prefix = '[DAP] ' })
+            larp.fn.map('n', '<leader>cdc', dap.continue, { desc = 'Continue', desc_prefix = '[DAP] ' })
+            larp.fn.map('n', '<F3>', dap.step_over, { desc = 'Step Over', desc_prefix = '[DAP] ' })
+            larp.fn.map('n', '<F4>', dap.step_into, { desc = 'Step Into', desc_prefix = '[DAP] ' })
         end,
     },
     {
--- lua/larp/plugins/utils.lua
+++ lua/larp/plugins/utils.lua
@@ -378,4 +378,16 @@ return {
         'NMAC427/guess-indent.nvim',
         opts = {},
     },
+    {
+        'rmagatti/auto-session',
+        lazy = false,
+
+        ---enables autocomplete for opts
+        ---@module "auto-session"
+        ---@type AutoSession.Config
+        opts = {
+            suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+            -- log_level = 'debug',
+        },
+    },
 }
