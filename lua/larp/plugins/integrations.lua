d42a8ef838c27a9e6a6bae199d20aca6733ac364
kjwsl <kjwdev01@gmail.com>
2024-11-09 16:51:04 +0900

    update: add flutter plugin

--- lua/larp/plugins/integrations.lua
+++ lua/larp/plugins/integrations.lua
@@ -243,4 +243,13 @@ return {
             })
         end,
     },
+    {
+        'nvim-flutter/flutter-tools.nvim',
+        ft = 'dart',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'stevearc/dressing.nvim', -- optional for vim.ui.select
+        },
+        config = true,
+    },
 }
