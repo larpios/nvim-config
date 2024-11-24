6be5dd2beaf473e78b6b3a067a19c8a84b723712
kjwsl <kjwdev01@gmail.com>
2024-11-24 23:40:36 +0900

    update: remove snacks.nvim. it's lagging

--- lua/larp/plugins/utils.lua
+++ lua/larp/plugins/utils.lua
@@ -200,22 +200,6 @@ return {
             prompt_end = '%$ ',
         },
     },
-    {
-        'folke/snacks.nvim',
-        priority = 1000,
-        lazy = false,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-            bigfile = { enabled = true },
-            notifier = { enabled = true },
-            quickfile = { enabled = true },
-            statuscolumn = { enabled = true },
-            words = { enabled = true },
-            dashboard = { enabled = true },
-        },
-    },
 }
 
 -- optional, highly recommended
