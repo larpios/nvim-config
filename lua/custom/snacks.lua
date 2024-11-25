48a35f114984176bb590061b6305d9833ba5dde8
kjwsl <kjwdev01@gmail.com>
2024-11-25 09:14:20 +0900

    update: use snacks again

---
+++ lua/custom/snacks.lua
@@ -0,0 +1,12 @@
+local opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    -- notifier = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+}
+
+require('snacks').setup(opts)
