8e5257a0758432e85ca434e3efd5825ddc25941f
kjwsl <kjwdev01@gmail.com>
2024-11-30 22:49:18 +0900

    update(snacks): enable notifier and add keymap

--- lua/custom/snacks.lua
+++ lua/custom/snacks.lua
@@ -3,10 +3,15 @@ local opts = {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    -- notifier = { enabled = true },
+    notifier = { enabled = true },
     quickfile = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
 }
 
-require('snacks').setup(opts)
+local snacks = require('snacks')
+local notifier = snacks.notifier
+snacks.setup(opts)
+
+
+larp.fn.map('n', '<leader>Nff', notifier.show_history, { desc = 'Show History', desc_prefix = 'Notifier' })
