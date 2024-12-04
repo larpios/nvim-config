97bcfec1d51e50449087c820382fa6b1741b7e11
kjwsl <kjwdev01@gmail.com>
2024-12-04 19:50:24 +0900

    update: add copilot chat plugin

--- lua/larp/plugins/editing.lua
+++ lua/larp/plugins/editing.lua
@@ -176,4 +176,16 @@ return {
 
         'HiPhish/rainbow-delimiters.nvim',
     },
+    {
+        'CopilotC-Nvim/CopilotChat.nvim',
+        dependencies = {
+            { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+            { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+        },
+        build = 'make tiktoken', -- Only on MacOS or Linux
+        opts = {
+            -- See Configuration section for options
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+    },
 }
