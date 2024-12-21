1d8619bade1aa005bea02e2396222c195d2a4abb
ray <kjwdev01@gmail.com>
2024-12-21 23:15:19 +0900

    update(blink.cmp): pin to v0.7.* until new version is stable

--- lua/larp/plugins/lsp.lua
+++ lua/larp/plugins/lsp.lua
@@ -160,7 +160,7 @@ return {
         },
 
         -- use a release tag to download pre-built binaries
-        version = 'v0.*',
+        version = 'v0.7.*',
         -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
         -- build = 'cargo build --release',
         -- On musl libc based systems you need to add this flag
