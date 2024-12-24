b116b39459e6c16c6d6af0bff88ee23c953ca398
ray <kjwdev01@gmail.com>
2024-12-24 12:31:30 +0900

    update: add lazydev plugin

--- lua/custom/blink.lua
+++ lua/custom/blink.lua
@@ -37,7 +37,7 @@ local opts = {
         },
     },
     sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer', 'ripgrep', 'copilot' },
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'ripgrep', 'copilot', 'lazydev' },
         providers = {
             ripgrep = {
                 module = 'blink-ripgrep',
@@ -68,6 +68,12 @@ local opts = {
                 module = 'blink-cmp-copilot',
                 score_offset = 100,
                 async = true,
+            },
+            lazydev = {
+                name = 'LazyDev',
+                module = 'lazydev.integrations.blink',
+                -- make lazydev completions top priority (see `:h blink.cmp`)
+                score_offset = 100,
             },
         },
     },
