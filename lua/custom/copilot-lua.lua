ed13f0f9fcba9671bfc89ca592f4333c14aa8c90
ray <kjwdev01@gmail.com>
2025-01-18 17:16:03 +0900

    update(copilot): remap "accept" from tab to <c-j>

--- lua/custom/copilot-lua.lua
+++ lua/custom/copilot-lua.lua
@@ -7,3 +7,8 @@ local options = {
 
 copilot.setup(options)
 
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+    expr = true,
+    replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
