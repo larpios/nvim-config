ac7e205e7856c67cf47f813d6fcca5ddd69cd483
ray <kjwdev01@gmail.com>
2025-02-04 08:25:30 +0900

    update(snacks): add more submodules and add keymaps

--- lua/custom/snacks.lua
+++ lua/custom/snacks.lua
@@ -3,16 +3,23 @@ local opts = {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
+    bufdelete = {}, -- Delete buffers without disrupting window layout.
+    indent = { enabled = true },
+    input = {}, -- Better `vim.ui.input`.
+    picker = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
+    git = {}, -- Git utilities.
+    gitbrowse = {}, -- Open the repo of the active file in the browser.
     statuscolumn = { enabled = true },
-    words = { enabled = true },
     toggle = {},
+    scope = {}, -- Scope detection based on treesitter or indent.
+    words = { enabled = true },
 }
 
 local snacks = require('snacks')
 local notifier = snacks.notifier
 snacks.setup(opts)
 
-
 larp.fn.map('n', '<leader>Nff', notifier.show_history, { desc = 'Show History', desc_prefix = 'Notifier' })
+larp.fn.map('n', '<leader>Gro', snacks.gitbrowse.open, { desc = 'Open in Browser', desc_prefix = 'Git' })
