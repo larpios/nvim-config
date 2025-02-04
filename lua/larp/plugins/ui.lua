8f196a76cf8f112d290e623a9960bac198405921
ray <kjwdev01@gmail.com>
2025-02-04 12:18:55 +0900

    update(which-key): change layout to "modern"

--- lua/larp/plugins/ui.lua
+++ lua/larp/plugins/ui.lua
@@ -123,11 +123,9 @@ return {
     {
         'folke/which-key.nvim',
         event = 'VeryLazy',
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
+        config = function()
+            require('custom.which-key')
+        end
     },
     {
         'folke/zen-mode.nvim',
