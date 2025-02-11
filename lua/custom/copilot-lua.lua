5e93948efa029afc1b1ca6ba397980b42f8182bc
ray <kjwdev01@gmail.com>
2025-02-12 07:33:13 +0900

    update(copilot): change accept keybinding to c-a

--- lua/custom/copilot-lua.lua
+++ lua/custom/copilot-lua.lua
@@ -7,8 +7,9 @@ local options = {
 
 copilot.setup(options)
 
-vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+vim.keymap.set('i', '<C-A>', 'copilot#Accept("\\<CR>")', {
     expr = true,
     replace_keycodes = false
 })
 vim.g.copilot_no_tab_map = true
+
