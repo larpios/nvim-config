3a81bdc256d979f81bb93b52890336a9ed8b52fd
ray <kjwdev01@gmail.com>
2025-03-21 23:27:21 +0900

    update(colorscheme): add configs to tokyonight

--- lua/larp/init.lua
+++ lua/larp/init.lua
@@ -4,3 +4,5 @@ require('larp.third-party')
 require('larp.lazy')
 -- lazy loads this
 -- require('larp.plugins')
+
+vim.cmd.colorscheme('tokyonight-moon')
--- lua/larp/plugins/colorschemes.lua
+++ lua/larp/plugins/colorschemes.lua
@@ -11,10 +11,11 @@ return {
         'folke/tokyonight.nvim',
         lazy = false,
         priority = 1000,
-        opts = {},
-        config = function()
-            vim.cmd.colorscheme('tokyonight-moon')
-        end,
+        opts = {
+            -- transparent = true,
+            dim_inactive = true,
+            lualine_bold = true,
+        },
     },
     --
     -- {
