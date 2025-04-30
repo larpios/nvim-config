260601eeb8c34ea414450ebc6e215819c15a58e0
ray <kjwdev01@gmail.com>
2025-05-01 00:21:10 +0900

    update(plugins): add tiny-code-action

--- lua/larp/plugins/utils.lua
+++ lua/larp/plugins/utils.lua
@@ -206,6 +206,18 @@ return {
         'ZWindL/orphans.nvim',
         opts = {},
     },
+    {
+        "rachartier/tiny-code-action.nvim",
+        dependencies = {
+            { "nvim-lua/plenary.nvim" },
+            { "ibhagwan/fzf-lua" },
+        },
+        event = "LspAttach",
+        keys = {
+            { "<leader>cA", function() require('tiny-code-action').code_action() end, mode = { 'n', 'v' }, desc = 'Code Action' },
+        },
+        opts = {},
+    }
 }
 
 -- optional, highly recommended
