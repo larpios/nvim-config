715938cd5811a313307f5f095dcb52da66406d71
ray <kjwdev01@gmail.com>
2025-05-23 08:49:30 +0900

    feat(colorscheme): remove onedark.nvim

--- lua/larp/plugins/colorschemes.lua
+++ lua/larp/plugins/colorschemes.lua
@@ -72,17 +72,17 @@ return {
             lualine_bold = true,
         },
     },
-    {
-        'navarasu/onedark.nvim',
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            require('onedark').setup({
-                style = 'darker',
-            })
-            -- Enable theme
-            require('onedark').load()
-        end,
-    },
+    -- {
+    --     'navarasu/onedark.nvim',
+    --     priority = 1000, -- make sure to load this before all the other start plugins
+    --     config = function()
+    --         require('onedark').setup({
+    --             style = 'darker',
+    --         })
+    --         -- Enable theme
+    --         require('onedark').load()
+    --     end,
+    -- },
     --
     -- {
     --     'mawkler/onedark.nvim',
