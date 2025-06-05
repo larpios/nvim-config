442e6077c655bac202bb7dd4fc06289dd4b40143
ray <kjwdev01@gmail.com>
2025-06-05 22:54:09 +0900

    chore(plugins): update config for avante

--- lua/larp/plugins/ai.lua
+++ lua/larp/plugins/ai.lua
@@ -51,16 +51,18 @@ return {
         version = false, -- Never set this value to "*"! Never!
         opts = {
             -- add any opts here
-            -- for example
             provider = 'claude',
-            openai = {
-                endpoint = 'https://api.anthropic.com',
-                model = 'claude-4-sonnet-20240513', -- your desired model (or use gpt-4o, etc.)
-                timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-                temperature = 0,
-                max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-                --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-            },
+            providers = {
+                openai = {
+                    endpoint = 'https://api.anthropic.com',
+                    model = 'claude-4-sonnet-20240513', -- your desired model (or use gpt-4o, etc.)
+                    timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+                    extra_request_body = {
+                        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+                        temperature = 0,
+                    }
+                },
+            }
         },
         -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
         build = 'make',
