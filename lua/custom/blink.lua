838f4a0cae8ab1279c8b3c3957713a647ced1f36
ray <kjwdev01@gmail.com>
2025-07-12 15:16:09 +0900

    fix: annoying errors from codeium when writing to an oil.nvim buffer

--- lua/custom/blink.lua
+++ lua/custom/blink.lua
@@ -71,6 +71,15 @@ local opts = {
             },
             codeium = {
                 name = 'Codeium',
+                enabled = function()
+                    local path = vim.api.nvim_buf_get_name(0)
+
+                    if string.find(path, "oil://", 1, true) == 1 then
+                        return false
+                    end
+
+                    return true
+                end,
                 module = 'codeium.blink',
                 async = true,
             },
