e3cc0a35c31189ad05ad0b1f44f3988081c0fb3e
ray <kjwdev01@gmail.com>
2025-08-11 01:11:36 +0900

    feat(transparent.lua): only remove background color of snacks picker

--- lua/larp/plugins/colorschemes.lua
+++ lua/larp/plugins/colorschemes.lua
@@ -78,40 +78,7 @@ return {
         -- Modify this to enable/disable transparent background
         enabled = true,
         config = function()
-            local transparent = require('transparent')
-            local opts = {
-                groups = {
-                    'Normal',
-                    'NormalNC',
-                    'Comment',
-                    'Constant',
-                    'Special',
-                    'Identifier',
-                    'Statement',
-                    'PreProc',
-                    'Type',
-                    'Underlined',
-                    'Todo',
-                    'String',
-                    'Function',
-                    'Conditional',
-                    'Repeat',
-                    'Operator',
-                    'Structure',
-                    'LineNr',
-                    'NonText',
-                    'SignColumn',
-                    'CursorLine',
-                    'CursorLineNr',
-                    'StatusLine',
-                    'StatusLineNC',
-                    'EndOfBuffer',
-                },
-            }
-
-            transparent.setup(opts)
-            transparent.clear_prefix('Snacks')
-            transparent.clear_prefix('SnacksPicker')
+            require('custom.transparent')
         end,
     },
 
