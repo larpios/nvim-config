f4b41db48670475d9187d51245837724baed0d91
ray <kjwdev01@gmail.com>
2025-10-14 08:34:45 +0900

    fix: nvim-ufo causing end of buffer column signs to disappear

--- lua/larp/plugins/lsp.lua
+++ lua/larp/plugins/lsp.lua
@@ -68,11 +68,10 @@ return {
         event = 'BufRead',
         dependencies = { 'kevinhwang91/promise-async' },
         init = function()
-            vim.o.foldcolumn = '0'
+            vim.o.foldcolumn = '1'
             vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
             vim.o.foldlevelstart = 99
             vim.o.foldenable = true
-            vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
         end,
         config = function()
             require('custom.nvim-ufo')
