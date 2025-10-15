7cfa31d142ddd241521e504a2e97075b8d3e034d
ray <kjwdev01@gmail.com>
2025-10-15 23:22:23 +0900

    feat: add CopilotChat.nvim back again

--- lua/larp/plugins/ai.lua
+++ lua/larp/plugins/ai.lua
@@ -11,6 +11,18 @@ return {
         end,
     },
     {
+        {
+            'CopilotC-Nvim/CopilotChat.nvim',
+            dependencies = {
+                { 'nvim-lua/plenary.nvim', branch = 'master' },
+            },
+            build = 'make tiktoken',
+            opts = {
+                -- See Configuration section for options
+            },
+        },
+    },
+    {
         'yetone/avante.nvim',
         event = 'VeryLazy',
         version = false, -- Never set this value to "*"! Never!
