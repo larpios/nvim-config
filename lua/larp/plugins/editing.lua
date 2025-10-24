3fbbcf3325922f4122c28f59c27d0ec306f84049
ray <kjwdev01@gmail.com>
2025-10-25 00:53:25 +0900

    feat: add undotree

--- lua/larp/plugins/editing.lua
+++ lua/larp/plugins/editing.lua
@@ -94,4 +94,15 @@ return {
             },
         },
     },
+    {
+        'jiaoshijie/undotree',
+        ---@module 'undotree.collector'
+        ---@type UndoTreeCollector.Opts
+        opts = {
+            -- your options
+        },
+        keys = { -- load the plugin only when using it's keybinding:
+            { '<leader>tu', "<cmd>lua require('undotree').toggle()<cr>" },
+        },
+    },
 }
