8903f6bd9adfadfe7bafac34ce9e85350dad76e9
ray <kjwdev01@gmail.com>
2026-03-15 22:30:44 +0900

    feat: add dap configuration from scratch

--- lua/larp/plugins/dev.lua
+++ lua/larp/plugins/dev.lua
@@ -43,41 +43,10 @@ return {
         end,
     },
     {
-        'mfussenegger/nvim-dap',
-        cmd = {
-            'DapNew',
-            'DapEval',
-            'DapContinue',
-            'DapDisconnect',
-            'DapStepInto',
-            'DapStepOut',
-            'DapShowLog',
-        },
-        event = 'BufRead',
-        dependencies = {
-            {
-                'rcarriga/nvim-dap-ui',
-                dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
-            },
-            {
-                'nvim-treesitter/nvim-treesitter',
-                build = ':TSUpdate',
-            },
-            'theHamsta/nvim-dap-virtual-text',
-        },
-        config = function()
-            require('custom.nvim-dap')
-        end,
-    },
-    {
-        -- 1. Highlights TODO, FIXME, etc. in your code
-        -- 2. Provides a list of all the highlights in your project
         'folke/todo-comments.nvim',
         event = 'BufRead',
         dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            require('custom.todo-comments')
-        end,
+        opts = {},
     },
     {
         'folke/lazydev.nvim',
