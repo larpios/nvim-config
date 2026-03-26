e5ab9c1caf48556168e421ddc7a577e712f0b7cf
ray <kjwdev01@gmail.com>
2026-03-26 23:59:27 +0900

    feat: add codecompanion.nvim

--- lua/larp/plugins/ai.lua
+++ lua/larp/plugins/ai.lua
@@ -6,7 +6,7 @@ return {
         cmd = 'NeoCodeium',
         keys = {
             {
-                '<A-a>',
+                '<A-f>',
                 function()
                     require('neocodeium').accept()
                 end,
@@ -31,6 +31,46 @@ return {
         opts = {},
     },
     {
+        "olimorris/codecompanion.nvim",
+        version = "^19.0.0",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        keys = {
+            { '<leader>acc', function() require('codecompanion').chat() end,   desc = '[CodeCompanion] Chat' },
+            { '<leader>act', function() require('codecompanion').toggle() end, desc = '[CodeCompanion] Toggle' },
+        },
+        cmd = 'CodeCompanion',
+        opts = {
+            interactions = {
+                chat = {
+                    adapter = 'ollama',
+                    model = 'qwen2.5-coder:32b',
+                }
+            }
+        };
+        config = function(_, opts)
+            local cc = require('codecompanion')
+            cc.setup(opts)
+
+            vim.api.nvim_create_user_command('CodeCompanion', function(args)
+                local subcmd = args.fargs[1]
+
+                if subcmd == 'chat' then
+                    cc.chat()
+                elseif subcmd == 'toggle' then
+                    cc.toggle()
+                end
+            end, {
+                nargs = 1,
+                complete = function(arg_lead, cmd_line, cursor_pos)
+                    return { 'chat', 'toggle' }
+                end
+            })
+        end
+    },
+    {
         'nickjvandyke/opencode.nvim',
         version = '*', -- Latest stable release
         dependencies = {
@@ -41,7 +81,7 @@ return {
                 optional = true,
                 opts = {
                     input = {}, -- Enhances `ask()`
-                    picker = { -- Enhances `select()`
+                    picker = {  -- Enhances `select()`
                         actions = {
                             opencode_send = function(...)
                                 return require('opencode').snacks_picker_send(...)
