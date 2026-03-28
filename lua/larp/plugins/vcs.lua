f27455e4dd89a90bf2d0337bd93c0b58b69dbe9d
ray <kjwdev01@gmail.com>
2026-03-28 15:51:03 +0900

    fix(neojj): use my own patch for `opts.cwd` issue until the PR is merged

--- lua/larp/plugins/vcs.lua
+++ lua/larp/plugins/vcs.lua
@@ -255,7 +255,10 @@ return {
         },
     },
     {
-        'NicholasZolton/neojj',
+        -- TODO: Switch back to the upstream when the PR is merged
+        -- 'NicholasZolton/neojj',
+        'larpios/neojj',
+        branch ='fix/workspace-root-resolution',
         lazy = true,
         opts = {
             disable_line_numbers = false,
@@ -277,12 +280,14 @@ return {
             {
                 '<leader>jc',
                 function()
+                    -- TODO: Remove this hack when the PR is merged
                     -- HACK: Currently, without `_workspace_root` set, Neojj will refuse to open in the config directory, saying it's not a workspace.
                     -- `_workspace_root` is set [here](https://github.com/NicholasZolton/neojj/blob/fdb6b50a4c5d52849c0b1eb0d493ea6feb634f48/lua/neojj.lua#L81-L90),
                     -- and used [here](https://github.com/NicholasZolton/neojj/blob/fdb6b50a4c5d52849c0b1eb0d493ea6feb634f48/lua/neojj.lua#L136-L162)
+                    -- local config_dir = vim.fn.stdpath('config')
+                    -- require('neojj').open({ cwd = config_dir, _workspace_root = config_dir})
 
-                    local config_dir = vim.fn.stdpath('config')
-                    require('neojj').open({ cwd = config_dir, _workspace_root = config_dir})
+                    require('neojj').open({ cwd = vim.fn.stdpath('config') })
                 end,
                 desc = '[Neojj] Run Neojj in Neovim Config',
             },
