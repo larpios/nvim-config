071d0874d3ae5961b66f996b02cbb98d11c403a4
ray <kjwdev01@gmail.com>
2026-03-29 17:52:03 +0900

    chore: format oil.lua

--- lua/larpi/plugins/oil.lua
+++ lua/larpi/plugins/oil.lua
@@ -14,62 +14,62 @@ return {
             ['-'] = 'actions.parent',
             -- If you want to automatically change the directory when selecting a file
             -- ['<CR>'] = function()
-                --     require('oil').select(nil, function(err)
-                    --         if not err then
-                    --             local curdir = require('oil').get_current_dir()
-                    --             if curdir then
-                    --                 vim.cmd.lcd(curdir)
-                    --             end
-                    --         end
-                    --     end)
-                    -- end,
-                    -- ['-'] = function()
-                        --     require('oil.actions').parent.callback()
-                        --     vim.cmd.lcd(require('oil').get_current_dir())
-                        -- end,
-                        ['<C-v>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
-                        ['<C-g>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
-                        -- ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
-                        ['<C-p>'] = 'actions.preview',
-                        ['<C-c>'] = 'actions.close',
-                        ['<F5>'] = 'actions.refresh',
-                        ['_'] = 'actions.open_cwd',
-                        ['`'] = 'actions.cd',
-                        -- ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' },
-                        ['gs'] = 'actions.change_sort',
-                        ['gx'] = 'actions.open_external',
-                        ['g.'] = 'actions.toggle_hidden',
-                        ['g\\'] = 'actions.toggle_trash',
-                    },
-                    watch_for_changes = true,
-                    view_options = {
-                        show_hidden = true,
-                        sort = {
-                            { 'type', 'desc' },
-                            { 'name', 'asc' },
-                        },
-                    },
-                    columns = {
-                        'icon',
-                        'mtime',
-                        'size',
-                        'permissions',
-                    },
-                    win_options = {
-                        wrap = true,
-                        winbar = "%{v:lua.require('oil').get_current_dir()}",
-                    },
-                },
-                keys = {
-                    {
-                        '-',
-                        function()
-                            if vim.bo.buftype == '' then
-                                require('oil').open()
-                            end
-                        end,
-                        mode = 'n',
-                        desc = 'Open parent directory',
-                    },
-                },
-            }
+            --     require('oil').select(nil, function(err)
+            --         if not err then
+            --             local curdir = require('oil').get_current_dir()
+            --             if curdir then
+            --                 vim.cmd.lcd(curdir)
+            --             end
+            --         end
+            --     end)
+            -- end,
+            -- ['-'] = function()
+            --     require('oil.actions').parent.callback()
+            --     vim.cmd.lcd(require('oil').get_current_dir())
+            -- end,
+            ['<C-v>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+            ['<C-g>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+            -- ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
+            ['<C-p>'] = 'actions.preview',
+            ['<C-c>'] = 'actions.close',
+            ['<F5>'] = 'actions.refresh',
+            ['_'] = 'actions.open_cwd',
+            ['`'] = 'actions.cd',
+            -- ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' },
+            ['gs'] = 'actions.change_sort',
+            ['gx'] = 'actions.open_external',
+            ['g.'] = 'actions.toggle_hidden',
+            ['g\\'] = 'actions.toggle_trash',
+        },
+        watch_for_changes = true,
+        view_options = {
+            show_hidden = true,
+            sort = {
+                { 'type', 'desc' },
+                { 'name', 'asc' },
+            },
+        },
+        columns = {
+            'icon',
+            'mtime',
+            'size',
+            'permissions',
+        },
+        win_options = {
+            wrap = true,
+            winbar = "%{v:lua.require('oil').get_current_dir()}",
+        },
+    },
+    keys = {
+        {
+            '-',
+            function()
+                if vim.bo.buftype == '' then
+                    require('oil').open()
+                end
+            end,
+            mode = 'n',
+            desc = 'Open parent directory',
+        },
+    },
+}
