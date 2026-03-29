3a820f73d9f64edc25afa8ed787771517eee238f
ray <kjwdev01@gmail.com>
2026-03-30 00:04:51 +0900

    feat: use several mini.nvim modules

--- lua/larpi/plugins/mini.lua
+++ lua/larpi/plugins/mini.lua
@@ -26,9 +26,21 @@ return {
         --
         require('mini.tabline').setup({})
 
+        -- Better 'f' and 't' motions
+        require('mini.jump').setup({})
+
+        -- Hit `Enter` to jump to a hinted position
+        require('mini.jump2d').setup({})
+
+        vim.api.nvim_set_hl(0, 'MiniJump', { link = 'Visual' })
+
         -- mini.ai is a module that provides more text objects, especially for ones that start with `a(round)`, and `i(nside)`
         -- Check out the documentation for more information (https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-ai.md)
-        require('mini.ai').setup({})
+        require('mini.ai').setup({
+            n_lines = 1000,
+            search_method = 'cover_or_nearest',
+        })
+
         require('mini.diff').setup({
             mappings = {
                 apply = 'gq',
@@ -41,17 +53,24 @@ return {
                 goto_last = ']H',
             },
         })
+
+        -- Handle surround operations, like adding, deleting, changing, etc
         require('mini.surround').setup({
             respect_selection_type = true,
         })
-        require('mini.cmdline').setup()
+
+        require('mini.basics').setup({})
+        -- Autocomplete for command line
+        require('mini.cmdline').setup({})
+        -- Highlight word under cursor
+        require('mini.cursorword').setup({})
         require('mini.move').setup({
             mappings = {
                 -- In Visual Mode
                 left = '<leader>mh',
                 right = '<leader>ml',
                 down = '<leader>mj',
-                up = '<leader>mn',
+                up = '<leader>mk',
 
                 -- In Normal Mode
                 line_left = '<leader>mh',
