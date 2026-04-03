008e977040fe8ff007c35bf1157595067a3a929e
larpios <33772093+larpios@users.noreply.github.com>
2026-04-03 13:54:15 +0900

    fix(conform): remove stop_ater_first

--- lua/larpi/plugins/treesitter.lua
+++ lua/larpi/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    event = { 'BufReadPre', 'BufNewFile', 'SessionLoadPost' },
+    event = { 'BufReadPre', 'BufNewFile' },
     branch = 'main',
     build = ':TSUpdate',
 }
