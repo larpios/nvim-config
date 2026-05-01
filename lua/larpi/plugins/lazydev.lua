f8dff1e00a19df58b88ad3e25577dc7ad7a9987f
larpios <33772093+larpios@users.noreply.github.com>
2026-05-01 21:54:26 +0900

    fix: use lazydev instead of relying on .luarc

--- lua/larpi/plugins/lazydev.lua
+++ lua/larpi/plugins/lazydev.lua
@@ -6,6 +6,8 @@ return {
             -- See the configuration section for more details
             -- Load luvit types when the `vim.uv` word is found
             { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+            { path = 'snacks.nvim', words = { 'Snacks' } },
+            { path = vim.fn.stdpath('config'), words = { 'larpi' } },
         },
     },
 }
